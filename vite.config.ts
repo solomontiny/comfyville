bd2a7ae9516d02e506acbe1929252111117f2f9e
Solomon <youremail@example.com>
2026-04-13 16:21:03 +0100

    fix vite base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   base: "/comfyville/",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 });

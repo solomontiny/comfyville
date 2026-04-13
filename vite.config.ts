97f547e0ee2ab12a0bd732544febf1dc43c811e9
Solomon <youremail@example.com>
2026-04-13 17:29:17 +0100

    fix base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/comfyville/",
+  base: "/", // ✅ IMPORTANT for custom domain
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
